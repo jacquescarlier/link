b7b5b28b51f4afdf23cdad00cee061af04d1d763
jacquescarlier <jacques.carlier@gmail.com>
2025-11-07 21:29:03 +0100

    img optimization

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Navi />
-      <section className="svodSection">
+      <section>
         <h2>SVOD & TV </h2>
         <Card raccourcis={raccourcisSvod} />
       </section>
--- src/data/data.js
+++ src/data/data.js
@@ -2,70 +2,70 @@ export const raccourcisLinux = [
     {
         "id": "l1231",
         "title": "DistroWatch",
-        "cover": "./linux/distrowatch.jpeg",
+        "cover": "./linux/distrowatch.webp",
         "altText": "distroWatch",
         "link": "https://distrowatch.com/"
     },
     {
         "id": "l1232",
         "title": "Debian_Forum",
-        "cover": "./linux/debianforum.png",
+        "cover": "./linux/debianforum.webp",
         "altText": "debian_forum",
         "link": "https://forums.debian.net/"
     },
     {
         "id": "l1233",
         "title": "Debian_Facile",
-        "cover": "./linux/debianfacile.png",
+        "cover": "./linux/debianfacile.webp",
         "altText": "debian_facile",
         "link": "https://debian-facile.org/"
     },
     {
         "id": "l1234",
         "title": "Linux_Mint",
-        "cover": "./linux/Linuxmint.png",
+        "cover": "./linux/Linuxmint.webp",
         "altText": "Linux_Mint",
         "link": "https://linuxmint.com/"
     },
     {
         "id": "l1235",
         "title": "Ubuntu",
-        "cover": "./linux/ubuntu.png",
+        "cover": "./linux/ubuntu.webp",
         "altText": "Ubuntu",
         "link": "https://www.ubuntu-fr.org/"
     },
     {
         "id": "l1236",
         "title": "OsNews",
-        "cover": "./linux/osnews.png",
+        "cover": "./linux/osnews.webp",
         "altText": "OsNews",
         "link": "https://www.osnews.com/"
     },
     {
         "id": "l1237",
         "title": "Lwn",
-        "cover": "./linux/lwn.png",
+        "cover": "./linux/lwn.webp",
         "altText": "Lwn.net",
         "link": "https://lwn.net/Articles/1033474/"
     },
     {
         "id": "l1238",
         "title": "Alternativalinux",
-        "cover": "./linux/alternativa.png",
+        "cover": "./linux/alternativa.webp",
         "altText": "Alternativalinux",
         "link": "https://www.alternativalinux.it/"
     },
     {
         "id": "l1239",
         "title": "Linux.com",
-        "cover": "./linux/linuxcom.png",
+        "cover": "./linux/linuxcom.webp",
         "altText": "Linux.com",
         "link": "https://www.linux.com/"
     },
     {
         "id": "l1240",
         "title": "Lea-linux",
-        "cover": "./linux/lealinux.jpeg",
+        "cover": "./linux/lealinux.webp",
         "altText": "Lea-linux",
         "link": "https://lea-linux.org/"
     },
@@ -78,49 +78,49 @@ export const raccourcisSvod = [
     {
         "id": "a1231",
         "title": "Orange_TV",
-        "cover": "./svod/orangetv.png",
+        "cover": "./svod/orangetv.webp",
         "altText": "Orange_Tv",
         "link": "https://tv.orange.fr/"
     },
     {
         "id": "a1232",
         "title": "Free_TV",
-        "cover": "./svod/freetv.png",
+        "cover": "./svod/freetv.webp",
         "altText": "Free_Tv",
         "link": "https://tv.free.fr/home/channels"
     },
     {
         "id": "a1233",
         "title": "Prime_Video",
-        "cover": "./svod/primevideo.png",
+        "cover": "./svod/primevideo.webp",
         "altText": "Prime_Video",
         "link": "https://www.primevideo.com/region/eu/offers/nonprimehomepage/ref=dvm_crs_prm_fr_s_a_ypf_eg_al_uc"
     },
     {
         "id": "a1234",
         "title": "YouTube",
-        "cover": "./svod/youtube.png",
+        "cover": "./svod/youtube.webp",
         "altText": "YouTube",
         "link": "https://www.youtube.com/"
     },
     {
         "id": "a1235",
         "title": "Rakuten_Viki",
-        "cover": "./svod/rakutenviki.png",
+        "cover": "./svod/rakutenviki.webp",
         "altText": "Rakuten_viki",
         "link": "https://www.viki.com/?locale=fr"
     },
     {
         "id": "a1236",
         "title": "Netflix",
-        "cover": "./svod/netflix.png",
+        "cover": "./svod/netflix.webp",
         "altText": "Netflix",
         "link": "https://www.netflix.com/fr/"
     },
     {
         "id": "a1237",
         "title": "Disney",
-        "cover": "./svod/disney-icon.png",
+        "cover": "./svod/disney-icon.webp",
         "altText": "Disney",
         "link": "https://www.viki.com/?locale=fr"
     },
@@ -130,70 +130,70 @@ export const raccourcisAstuces = [
     {
         "id": "A1231",
         "title": "Korben",
-        "cover": "./astuces/korben.png",
+        "cover": "./astuces/korben.webp",
         "altText": "Korben",
         "link": "https://korben.info/"
     },
     {
         "id": "A1232",
         "title": "Malekal",
-        "cover": "./astuces/malekal.png",
+        "cover": "./astuces/malekal.webp",
         "altText": "Malekal",
         "link": "https://www.malekal.com/"
     },
     {
         "id": "A1233",
         "title": "le_crabe_info",
-        "cover": "./astuces/lecrabeinfo.png",
+        "cover": "./astuces/lecrabeinfo.webp",
         "altText": "le_crabe_info",
         "link": "https://lecrabeinfo.net/"
     },
     {
         "id": "A1234",
         "title": "PC_astuces",
-        "cover": "./astuces/pcastuces.png",
+        "cover": "./astuces/pcastuces.webp",
         "altText": "PC_astuces",
         "link": "https://www.pcastuces.com/"
     },
     {
         "id": "A1235",
         "title": "Just_Geek",
-        "cover": "./astuces/justgeek.jpeg",
+        "cover": "./astuces/justgeek.webp",
         "altText": "Just_Geek",
         "link": "https://www.justgeek.fr/"
     },
     {
         "id": "A1236",
         "title": "Developpez.com",
-        "cover": "./astuces/developpezcom.jpeg",
+        "cover": "./astuces/developpezcom.webp",
         "altText": "Developpez.com",
         "link": "https://www.developpez.com/"
     },
     {
         "id": "A1237",
         "title": "Stack_Overflow",
-        "cover": "./astuces/stackoverflow.png",
+        "cover": "./astuces/stackoverflow.webp",
         "altText": "Stack_OverFlow",
         "link": "https://stackoverflow.com/questions"
     },
     {
         "id": "A1238",
         "title": "ChatGPT",
-        "cover": "./astuces/chatgpt.png",
+        "cover": "./astuces/chatgpt.webp",
         "altText": "ChatGPT",
         "link": "https://chatgpt.com/"
     },
     {
         "id": "A1239",
         "title": "Mistral.ai",
-        "cover": "./astuces/mistralai.png",
+        "cover": "./astuces/mistralai.webp",
         "altText": "Mistral.ai",
         "link": "https://mistral.ai/fr"
     },
     {
         "id": "A1240",
         "title": "Claude.ai",
-        "cover": "./astuces/claude.png",
+        "cover": "./astuces/claude.webp",
         "altText": "Claude.ai",
         "link": "https://claude.ai/login?returnTo=%2F%3F"
     },
@@ -203,70 +203,70 @@ export const raccourcisPresses = [
     {
         "id": "P1231",
         "title": "Les_numériques",
-        "cover": "./presses/lesnumeriques.jpg",
+        "cover": "./presses/lesnumeriques.webp",
         "altText": "Les_numériques",
         "link": "https://www.lesnumeriques.com/"
     },
     {
         "id": "P1232",
         "title": "Phonandroid",
-        "cover": "./presses/phonandroid.png",
+        "cover": "./presses/phonandroid.webp",
         "altText": "Phonandroid",
         "link": "https://www.phonandroid.com/"
     },
     {
         "id": "P1233",
         "title": "tom's_guide",
-        "cover": "./presses/tomsguide.jpg",
+        "cover": "./presses/tomsguide.webp",
         "altText": "tom's_guide",
         "link": "https://www.tomsguide.fr/"
     },
     {
         "id": "P1234",
         "title": "LaptopSpirit",
-        "cover": "./presses/laptopspirit.jpeg",
+        "cover": "./presses/laptopspirit.webp",
         "altText": "Laptop_Spirit",
         "link": "https://www.laptopspirit.fr/"
     },
     {
         "id": "P1235",
         "title": "CowCotland",
-        "cover": "./presses/cowcotland.jpeg",
+        "cover": "./presses/cowcotland.webp",
         "altText": "CowCotland",
         "link": "https://www.cowcotland.com/"
     },
     {
         "id": "P1236",
         "title": "Presse-citron",
-        "cover": "./presses/pressecitron.png",
+        "cover": "./presses/pressecitron.webp",
         "altText": "Presse-citron",
         "link": "https://www.presse-citron.net/"
     },
     {
         "id": "P1237",
         "title": "Frandroid",
-        "cover": "./presses/frandroid.png",
+        "cover": "./presses/frandroid.webp",
         "altText": "Frandroid",
         "link": "https://www.frandroid.com/"
     },
     {
         "id": "P1238",
         "title": "Next",
-        "cover": "./presses/next.png",
+        "cover": "./presses/next.webp",
         "altText": "Next",
         "link": "https://next.ink/"
     },
     {
         "id": "P1239",
         "title": "Clubic",
-        "cover": "./presses/clubic.png",
+        "cover": "./presses/clubic.webp",
         "altText": "Clubic",
         "link": "https://www.clubic.com/"
     },
     {
         "id": "P1240",
         "title": "ZdNet",
-        "cover": "./presses/zdnet.png",
+        "cover": "./presses/zdnet.webp",
         "altText": "ZdNet",
         "link": "https://www.zdnet.fr/"
     },
@@ -275,77 +275,77 @@ export const raccourcisDivers = [
     {
         "id": "D1231",
         "title": "Wikipédia",
-        "cover": "./divers/wikipedia.jpeg",
+        "cover": "./divers/wikipedia.webp",
         "altText": "Wikipédia",
         "link": "https://fr.wikipedia.org/wiki/Wiki"
     },
     {
         "id": "D1232",
         "title": "Github",
-        "cover": "./divers/github.jpeg",
+        "cover": "./divers/github.webp",
         "altText": "Github",
         "link": "https://github.com/"
     },
     {
         "id": "D1233",
         "title": "Netlify",
-        "cover": "./divers/netlify.jpeg",
+        "cover": "./divers/netlify.webp",
         "altText": "Netlify",
         "link": "https://app.netlify.com/teams/jacquescarlier/projects"
     },
     {
         "id": "D1234",
         "title": "Vercel",
-        "cover": "./divers/vercel.jpeg",
+        "cover": "./divers/vercel.webp",
         "altText": "Vercel",
         "link": "https://vercel.com/"
     },
     {
         "id": "D1235",
         "title": "Nperf",
-        "cover": "./divers/nperf.png",
+        "cover": "./divers/nperf.webp",
         "altText": "Nperf",
         "link": "https://www.nperf.com/fr/"
     },
     {
         "id": "D1236",
         "title": "SpeedTest",
-        "cover": "./divers/speedtest.png",
+        "cover": "./divers/speedtest.webp",
         "altText": "SpeedTest",
         "link": "https://www.speedtest.net/fr"
     },
     {
         "id": "D1237",
         "title": "BrowserScan",
-        "cover": "./divers/browserscan.jpeg",
+        "cover": "./divers/browserscan.webp",
         "altText": "BrowserScan",
         "link": "https://www.browserscan.net/fr/dns-leak"
     },
     {
         "id": "D1238",
         "title": "DNS leak test",
-        "cover": "./divers/dnsleaktest.png",
+        "cover": "./divers/dnsleaktest.webp",
         "altText": "DNS leak test",
         "link": "https://www.dnsleaktest.com/how-to-fix-a-dns-leak.html"
     },
     {
         "id": "D1239",
         "title": "CrownCloud Wiki",
-        "cover": "./divers/crowncloudwiki.jpg",
+        "cover": "./divers/crowncloudwiki.webp",
         "altText": "CrownCloud Wikit",
         "link": "https://wiki.crowncloud.net/index.php"
     },
     {
         "id": "D1240",
         "title": "Internet Archive",
-        "cover": "./divers/archiveinternet.png",
+        "cover": "./divers/archiveinternet.webp",
         "altText": "Internet Archive",
         "link": "https://archive.org/search?query=creator%3A%22Kate+Bush%22"
     },
 {
         "id": "D1241",
         "title": "Archives d'Anna",
-        "cover": "./divers/archivesdanna.png",
+        "cover": "./divers/archivesdanna.webp",
         "altText": "Archives d'Anna",
         "link": "https://fr.annas-archive.org/"
     },
