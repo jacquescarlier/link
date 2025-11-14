7afdf8e654e94d57927b7252e9e2576be7f6c543
jacquescarlier <jacques.carlier@gmail.com>
2025-11-14 22:28:38 +0100

    ADD <main> for accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,36 @@ function App() {
   return (
     <div className="parallax">
       <Navi />
-      <section>
-        <h2>SVOD & TV </h2>
-        <Card raccourcis={raccourcisSvod} />
-      </section>
-      <section>
-        <h2>Linux</h2>
-        <Card raccourcis={raccourcisLinux} />
-      </section>
-      <section>
-        <h2>Aides & Astuces</h2>
-        <Card raccourcis={raccourcisAstuces} />
-      </section>
-      <section>
-        <h2>Presses</h2>
-        <Card raccourcis={raccourcisPresses} />
-      </section>
-      <section>
-        <h2>Divers</h2>
-        <Card raccourcis={raccourcisDivers} />
-      </section>
+
+      <main>
+
+        <section>
+          <h2>SVOD & TV </h2>
+          <Card raccourcis={raccourcisSvod} />
+        </section>
+
+        <section>
+          <h2>Linux</h2>
+          <Card raccourcis={raccourcisLinux} />
+        </section>
+
+        <section>
+          <h2>Aides & Astuces</h2>
+          <Card raccourcis={raccourcisAstuces} />
+        </section>
+
+        <section>
+          <h2>Presses</h2>
+          <Card raccourcis={raccourcisPresses} />
+        </section>
+
+        <section>
+          <h2>Divers</h2>
+          <Card raccourcis={raccourcisDivers} />
+        </section>
+
+      </main>
+
       <footer>
         <span>
           copyright© JCR & P1t
