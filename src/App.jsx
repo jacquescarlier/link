59226382d2a36bb6ccd0b0deec77ab7745e7b3ff
jacquescarlier <jacques.carlier@gmail.com>
2025-11-14 22:44:03 +0100

    end day backup

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,31 @@ import "./App.css"
 import Navi from "./containers/nav/Navi";
 
 function App() {
-
-
   return (
     <div className="parallax">
       <Navi />
-
       <main>
-
         <section>
           <h2>SVOD & TV </h2>
           <Card raccourcis={raccourcisSvod} />
         </section>
-
         <section>
           <h2>Linux</h2>
           <Card raccourcis={raccourcisLinux} />
         </section>
-
         <section>
           <h2>Aides & Astuces</h2>
           <Card raccourcis={raccourcisAstuces} />
         </section>
-
         <section>
           <h2>Presses</h2>
           <Card raccourcis={raccourcisPresses} />
         </section>
-
         <section>
           <h2>Divers</h2>
           <Card raccourcis={raccourcisDivers} />
         </section>
-
       </main>
-
       <footer>
         <span>
           copyright© JCR & P1t
@@ -47,5 +37,4 @@ function App() {
     </div>
   )
 }
-
 export default App
